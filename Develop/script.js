6c241de4b89b772cdcb620630b57100cef6d3dc3
Nuseyba <n.kenadiid@gmail.com>
2022-11-02 11:13:15 +0000

    created a onclick function for button

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword () {
+const password =document.getElementById("")
+
+
+
+function generatePassword() {
+  console.log ("the button was clicked")
+}
+
+
   const charactersAmountCL = ["a","b","c","d","e","f","g","h","i","j,","k","l","m","n", "o","p","q","r,","s","t","u,","v","w","x","y","z"];
   const charactersAmountLC = ["A","B","C","D","E","F","G","H","I","J","K","L","M","M","N","O,","P","Q","R","S","T","U","V","W","X","Y","Z"];
   const charactersAmountNumbers = ["0,1,2,3,4,5,6,7,8,9"];
   const charactersAmountSymbols= ["!","@","£","$","%","%","_","-","<",">",];
 
-  const allDigits = [...charactersAmountCL,...charactersAmountLC,...charactersAmountNumbers,...charactersAmountSymbols]
-  console.log ("allDigits")
-}
-
-
 let passwordlength = prompt ("How many characters between 8 and 124 do you want in your password?");{
   if (isNaN(passwordlength))
-  alert ("no value. please enter a number.");
+  alert (`You have chosen ${passwordlength}charachters`)
+  prompt ("no value. please enter a number.");
   if (passwordlength <8 || passwordlength >128)
-  alert ("please enter a value between 8 and 128")
+  confirm ("please enter a value between 8 and 128")
 }
 
 const UserIncCL =confirm ("Do you want to add a capital letter?")
