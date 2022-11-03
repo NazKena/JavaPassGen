a0b77dfc88b8255d4d485a53eafb0be7fc52ded1
Nuseyba <n.kenadiid@gmail.com>
2022-11-03 18:26:29 +0000

    moved css and java script file into my assets folder

--- Assets/script.js
+++ Assets/script.js
@@ -7,19 +7,13 @@ function generatePassword() {
   console.log ("the button was clicked")
 }
 
-  const characters = ["a","b","c","d","e","f","g","h", "i","j","k","l","A","B","C","D","E","F","G","H","I","J","K","L","0","1","2","3","4","5","6","7","8","9","!","@","£","$","%","%",]
-
-  for (const i=0; i< passwordlength; i++) {
-    let randomNumber = Math.floor(Math.random()*characters.length);
-    password += characters.substring(randomNumber, randomNumber + 1)
-  }
+const characters = ["a","b","c","d","e","f","g","h", "i","j","k","l","A","B","C","D","E","F","G","H","I","J","K","L","0","1","2","3","4","5","6","7","8","9","!","@","£","$","%","%",]
 
 let passwordlength = prompt ("How many characters between 8 and 124 do you want in your password?");{
   if (isNaN(passwordlength))
-  alert (`You have chosen ${passwordlength}charachters`)
-  prompt ("no value. please enter a number.");
+  alert ("no value. please enter a number.");
   if (passwordlength <8 || passwordlength >128)
-  confirm ("please enter a value between 8 and 128")
+  alert ("please enter a value between 8 and 128")
 }
 
 const UserIncCL =confirm ("Do you want to add a capital letter?")
@@ -42,3 +36,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
