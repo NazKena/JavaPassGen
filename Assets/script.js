e16a509878d350cda6d14e09b822aa8b9e3ebb5d
Nuseyba <n.kenadiid@gmail.com>
2023-02-28 19:47:07 +0000

    changes

--- Assets/script.js
+++ Assets/script.js
@@ -9,12 +9,14 @@ function generatePassword() {
 
 const characters = ["a","b","c","d","e","f","g","h", "i","j","k","l","A","B","C","D","E","F","G","H","I","J","K","L","0","1","2","3","4","5","6","7","8","9","!","@","£","$","%","%",]
 
-let passwordlength = prompt ("How many characters between 8 and 124 do you want in your password?");{
-  if (isNaN(passwordlength))
-  alert ("no value. please enter a number.");
-  if (passwordlength <8 || passwordlength >128)
-  alert ("please enter a value between 8 and 128")
-}
+let pw= prompt ("Please choose a lenght between 8 and 128 characters");
+if (pw<8 || pw >128){
+  alert ("Incorrect password lenght")
+  generatePassword ()
+} else if (pw === '' || isNaN(pw)) {
+  alert ("Please enter valid password between 8 and 128")
+  generatePassword ()
+} 
 
 const UserIncCL =confirm ("Do you want to add a capital letter?")
 const UserIncLC = confirm ("Do you want to add a lowercase letter?")
@@ -26,9 +28,10 @@ for (const i=0; i< passwordlength; i++) {
 }
 
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password"); 
 
   passwordText.value = password;
 
